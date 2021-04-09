771396c728e2f705d0ae6cd6000c1d1c39a8e431
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-04-09 13:51:15 +0200

    .

--- RazorPageHotelAppUnitTest/HotelServiceTest.cs
+++ RazorPageHotelAppUnitTest/HotelServiceTest.cs
@@ -33,5 +33,35 @@ namespace RazorPageHotelAppUnitTest
 
             Assert.AreEqual(lastHotelNoBeforeAdd + 1, lastHotelNoAfterAdd);
         }
+
+        [TestMethod]
+        public void TestAddHotel()
+        {
+            //Arrange
+            HotelService hotelService = new HotelService(connectionString);
+            RoomService roomService = new RoomService(connectionString);
+            List<Hotel> hotels = hotelService.GetAllHotelAsync().Result;
+            
+
+            //Act
+            int numbersOfHotelsBefore = hotels.Count;
+            Hotel newHotel = new Hotel(1001, "TestHotel10293847", "Testvej");
+            bool ok = hotelService.CreateHotelAsync(newHotel).Result;
+            hotels = hotelService.GetAllHotelAsync().Result;
+
+            Hotel newHotelWithNewId = hotelService.GetHotelsByNameAsync("TestHotel10293847").Result[0];
+
+            int numbersOfHotelsAfter = hotels.Count;
+            List<Room> roomsInHotel = roomService.GetAllRoomsInHotelAsync(newHotel.HotelNr).Result;
+            Hotel h = hotelService.DeleteHotelAsync(newHotelWithNewId.HotelNr, roomsInHotel).Result;
+
+            //Assert
+            Assert.AreEqual(numbersOfHotelsBefore + 1, numbersOfHotelsAfter);
+            Assert.IsTrue(ok);
+            Assert.AreEqual(h.HotelNr, newHotelWithNewId.HotelNr);
+        }
+
     }
+
 }
+
